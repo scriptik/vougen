fe9036df85c744b46ff712553a8bd145fb7dbd6f
Pezhman Shafigh <pan@DellArch.localdomain>
2019-01-26 21:26:55 +0330

    password generator functions added

--- vougen.py
+++ vougen.py
@@ -17,3 +17,14 @@ import getpass
 
 pdfmetrics.registerFont(TTFont('DejaVuSans','DejaVuSans.ttf'))
 pdfmetrics.registerFont(TTFont('Arial','ariblk.ttf'))
+
+
+def password_generator(size=9, chars=string.ascii_letters + string.digits):
+    """
+    Returns a string of random characters, useful in generating temporary
+    passwords for automated password resets.
+
+    size: default=8; override to provide smaller/larger passwords
+    chars: default=A-Za-z0-9; override to provide more/less diversity
+    """
+    return ''.join(random.choice(chars) for i in range(size))

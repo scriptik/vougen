ddf895935de6dd08f50e2b4bcd09bd20c79b3b8c
Pezhman Shafigh <pan@DellArch.localdomain>
2019-01-26 21:23:18 +0330

    The modules imported

---
+++ vougen.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from reportlab.pdfgen import canvas
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from pathlib import Path
+import string
+import random
+import qrcode
+import os
+
+from librouteros import connect
+import ssl
+import time
+import getpass

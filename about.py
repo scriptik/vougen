94b2df7a44b176b1d10738fe2e68cf79d468875e
Pezhman Shafigh <pan@DellArch.localdomain>
2019-01-28 21:50:28 +0330

    initial commit

--- about.py
+++ about.py
@@ -14,6 +14,7 @@ mainWindow.grid()
 
 LICENCE = """MIT License
 
+Copyright (c) 2019 scriptik
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights

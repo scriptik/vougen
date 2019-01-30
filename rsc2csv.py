225c1fbc48b8649dff176af1bbad9b15ef712d6a
Pezhman Shafigh <pan@DellArch.localdomain>
2019-01-30 12:19:39 +0330

    extract data added

--- rsc2csv.py
+++ rsc2csv.py
@@ -1,6 +1,7 @@
 #### Convert the rsc File to csv 
 #### PEZHMAN SHAFIGH  2019
 import os
+import re
 import csv
 import datetime
 
@@ -18,34 +19,28 @@ num_id = num_lines-1
 lni = num_id*5 #last name id
 words = (string.split())
 #print(words)
+print(len(words))
+start = '='
+end = ','
 for i in range (5,lni,5):
     #print(words[i])
     #print(words[i+1])
     #print(words[i+2])
     #print(words[i-1])
-    print(words[i]+words[i+1]+words[i+2]+words[i-1])
-print(len(words))
-#num_date = len(words)+1
-#num_time = len(words)+8
-#x = range(1, num_date, 9)
-#y = range(8, num_time, 9)
-#
-#log_time = []
-#kb = []
-#for i in x:
-#    log_time.append(words[i])
-#for j in y:
-#    kb.append(words[j])
-#z = range(0, num_lines)
+    #print((words[i].split(start))[1].split(end)[0])
+    name = ((words[i].split(start))[1].split(end)[0])
+    password = ((words[i+1].split(start))[1].split(end)[0])
+    server = ((words[i+2].split(start))[1].split(end)[0])
+    litime = ((words[i-1].split(start))[1].split(end)[0])
+    instr = (name+","+password+","+server+","+litime)
+    #print(words[i]+","+words[i+1]+","+words[i+2]+","+words[i-1])
+    #instr = (words[i]+","+words[i+1]+","+words[i+2]+","+words[i-1])
+    print(instr)
 
-#now = datetime.datetime.now()
-##filename=str(now.year)+str(now.month)+str(now.day)
-#filename=now.strftime("%Y_%m_%d")+"RouterLOG"+".csv"
-##outputFile = open('output.csv', 'w', newline='')
-#outputFile = open(filename, 'w', newline='')
+#outputFile = open('output.csv', 'w', newline='')
 #outputWriter = csv.writer(outputFile)
-#outputWriter.writerow(['Year', 'Month', 'Day', 'Hour', 'Byte'])
-#
+#outputWriter.writerow(['Name', 'Password', 'Server', 'UpTime'])
+#outputWriter.writerow(instr)
 #for k in z:
 #    #print((log_time[k])[0:10]+','+(log_time[k])[11:19]+','+kb[k])
 #    csv_year = (log_time[k])[0:4]

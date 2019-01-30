c7f0f0f2a6b568263272e2f4ab405d7a1de5b64b
Pezhman Shafigh <pezhmanshafigh@gmail.com>
2019-01-30 18:04:00 +0330

    vougen imported to main

--- main.py
+++ main.py
@@ -10,7 +10,20 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
 import subprocess
-from vougen import vou_gen
+
+from reportlab.pdfgen import canvas
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from pathlib import Path
+import string
+import random
+import qrcode
+import os
+from librouteros import connect
+import ssl
+import time
+import getpass
+
 
 class vougen:
 
@@ -18,7 +31,7 @@ class vougen:
 
         self.master = master
         master.title("VOUGEN V1.0")
-        master.geometry('660x310+250+100')
+        master.geometry('660x410+250+100')
 
         #### Initialize Label Frames ####
 
@@ -156,25 +169,14 @@ class vougen:
         self.btndoit.configure(width="5")
         #self.btnQuit.configure(width="5")
 
+
     def BtnAbout(self):
         pr_about = subprocess.Popen([ 'python3', 'about.py' ])
 
     def Btndoit(self):
-        in01 = self.wtfi.get()
-        in02 = self.wtru.get()
-        in03 = int(self.sboxusnu.get())
-        in04 = self.entprefix.get()
-        in05 = int(self.cboxlenpass.get())
-        in06 = self.enthotdns.get()
-        in07 = self.cboxplt.get()
-        in08 = self.enthotnam.get()
-        in09 = self.entroupo.get()
-        in10 = self.entrouuse.get()
-        in11 = self.entroupass.get()
-        in12 = self.entrouip.get()
-        #vou_gen(wtofi = in01, idnu = in03, prefix = in04, size = in05, dnsname = in06, plt = in07, hotnam = in08)
-        vou_gen(in01,in02,in03,in04,in05,in06,in07,in08,in09,in10,in11,in12)
-        if in01 == 1:
+        self.vou_gen()
+        #self.textout.insert(END, vou_gen.roucomm)
+        if self.wtfi.get() == 1:
            print("user.rsc file created!")
            pr_csv = subprocess.Popen([ 'python3', 'rsc2csv.py' ])
 
@@ -193,6 +195,139 @@ class vougen:
 
     def BtnQuit(self):
         self.master.quit()
+
+    def callback(id, tex):
+        s = 'At {} f is {}\n'.format(id, id**id/0.987)
+        tex.insert(tk.END, s)
+        tex.see(tk.END)             # Scroll if necessary
+
+    def vou_gen(self):
+
+        chars=string.ascii_letters + string.digits
+        wtofi = self.wtfi.get()
+        wtoru = self.wtru.get()
+        idnu = int(self.sboxusnu.get())
+        prefix = self.entprefix.get()
+        size = int(self.cboxlenpass.get())
+        dnsname = self.enthotdns.get()
+        plt = self.cboxplt.get()
+        hotnam = self.enthotnam.get()
+        ru_po = self.entroupo.get()
+        ru_us = self.entrouuse.get()
+        ru_pa = self.entroupass.get()
+        ru_ip = self.entrouip.get()
+
+        pdfmetrics.registerFont(TTFont('DejaVuSans','DejaVuSans.ttf'))
+        pdfmetrics.registerFont(TTFont('Arial','ariblk.ttf'))
+
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=2,
+            border=4,
+        )
+
+        ############################################
+
+        filename = 'users.rsc'
+        if wtofi == 1:
+            for p in Path(".").glob("users.rsc"):
+                p.unlink()
+            with open(filename, 'w') as file_object:
+                file_object.write("/ip hotspot user\n")
+
+        if wtoru == 1:
+                print("Connecting to Router...")
+                ctx = ssl.create_default_context()
+                ctx.check_hostname = False
+                ctx.verify_mode = ssl.CERT_NONE
+                #api = connect(username='lab', password='1234', host='192.168.25.244', ssl_wrapper=ctx.wrap_socket, port=8729)
+                api = connect(username=ru_us, password=ru_pa, host=ru_ip, ssl_wrapper=ctx.wrap_socket, port=ru_po)
+
+        limit_time = plt+"d"
+        userdic = {}
+        for i in range(idnu):
+            user = prefix+str(i)
+            #passwd = (self.password_generator(size))
+            passwd = ''.join(random.choice(chars) for i in range(size))
+            userdic.update({user : passwd})
+            roucomm = ("add limit-uptime="+limit_time+" name="+user+" password="+passwd+" server="+hotnam)
+            print(roucomm)
+            if wtofi == 1:
+                with open(filename, 'a') as file_object:
+                    file_object.write(roucomm+"\n")
+            if wtoru == 1:
+                parms = {'limit-uptime': limit_time, 'name': user, 'password': passwd, 'server': 'hotspot1'}
+                api(cmd='/ip/hotspot/user/add', **parms)
+
+
+            qr.clear()
+            qr.add_data('http://'+dnsname+'/login?username='+user+'&password='+passwd)
+            qr.make(fit=True)
+
+            img = qr.make_image(fill_color="black", back_color="white")
+            #img.show()
+            img.save(user+".png")
+
+
+        print("Waiting ......")
+
+        ## Create the print temp
+        ptemp = "printtemp.jpg"
+        profile = plt+" DAY Access"
+        pnu = int(idnu/4)
+        minra = 0
+        maxra = 4
+        c = canvas.Canvas("vouchers.pdf")
+        for p in range(pnu):
+            c.setLineWidth(.1)
+            c.setFont('Helvetica', 12)
+
+            c.drawInlineImage(ptemp, 20,0, width=240,height=300)
+            c.drawInlineImage(ptemp, 320,0, width=240,height=300)
+            c.drawInlineImage(ptemp, 20,400, width=240,height=300)
+            c.drawInlineImage(ptemp, 320,400, width=240,height=300)
+
+
+
+            xqr = {0:160,1:460,2:160,3:460}
+            yqr = {0:505,1:505,2:105,3:105}
+            xus ={0:38,1:338,2:38,3:338}
+            yus ={0:555,1:555,2:155,3:155}
+            xps ={0:38,1:338,2:38,3:338}
+            yps ={0:515,1:515,2:115,3:115}
+
+            c.setFont("Arial", 13)
+            i = 0
+            for j in range(minra,maxra):
+                qrimage = prefix+str(j)+".png"
+                userq = prefix+str(j)
+                passq = (userdic[userq])
+                xqri = (xqr[i])
+                yqri = (yqr[i])
+                xusi = (xus[i])
+                yusi = (yus[i])
+                xpsi = (xps[i])
+                ypsi = (yps[i])
+                c.drawInlineImage(qrimage, xqri,yqri, width=None,height=None)
+                c.drawString(xusi,yusi, userq)
+                c.drawString(xpsi,ypsi, passq)
+                i += 1
+            minra += 4
+            maxra += 4
+            c.setFont("Arial", 10)
+            c.drawString(37,605, profile)
+            c.drawString(337,605, profile)
+            c.drawString(37,205, profile)
+            c.drawString(337,205, profile)
+            c.showPage()
+        c.save()
+        self.del_temp(prefix)
+
+    def del_temp(self, prefix):
+        for p in Path(".").glob(prefix+"*.png"):
+            p.unlink()
+        print("QR temp files Removed!")
 
 def main():
 
